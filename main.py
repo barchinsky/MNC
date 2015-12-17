b88ee4fa6f483791091df3f04f91ebaa6e1ce946
max <max@max-HP-ProBook-640-G1.(none)>
2015-12-17 12:23:19 +0200

    Project init. Added base server implementation using socket interface. Added test socket client.

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+	from src import Server
+except NameError as e:
+	print "Import module error:%s" %(e)
+
+def test():
+	print "Test";
+
+
+if __name__ == "__main__":
+	server = Server()
+	print server.getHost(), server.getPort()
+	server.start()
+	#print server.testHost("localhost")
---
+++ src/Server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env
+
+'''
+
+Server.py
+
+Interface developed for listening incoming requests from monitoring modules.
+
+'''
+
+try:
+	import socket
+	import ConfigParser
+except NameError as e:
+	print "Import error:%s"%(e)
+
+class Server():
+	def __init__(self):
+		self.config = ConfigParser.RawConfigParser();
+		self.config.read("conf/config.cfg")
+
+
+		self.host = self.config.get("server","host")
+		self.port = self.config.getint("server","port")
+		self.addr = (self.host, self.port)
+
+		self.server  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	def getHost(self):
+		return self.host
+
+	def getPort(self):
+		return self.port
+
+	def testHost(self, name):
+		return socket.gethostbyname(name)
+
+	def start(self):
+		self.server.bind( self.addr )
+		self.server.listen(1000)
+
+		print "Server started at: %s:%d"%(self.host, self.port)
+
+		while True:
+			conn, addr = self.server.accept()
+
+			data = conn.recv(1000)
+
+			if data:
+				print "Data received:%s from %s"%(data,addr)
+
+
+			conn.send('{"data":"ok"}')
+
+
