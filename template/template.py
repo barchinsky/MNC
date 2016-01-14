f20e910fbf37d81b60bcc48bf9a8ae03f30043ae
max <max@max-HP-ProBook-640-G1.(none)>
2016-01-14 17:40:12 +0200

    Cleaning.

--- template/template.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-RESPONSE = "{u'header': {u'type': '$rtype',u'status': '$status',u'sourceName': '$sourceName',u'sourceID': '$sourceID'},u'payload': {u'data': '$data'}}"
-TEST_REQUEST = '{"header": {"type": "test","sourceName": "test","sourceID": "test1"},"payload": {"data": "Test"}}'
